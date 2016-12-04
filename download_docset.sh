be384763b214d8548e42bd19b30840a5fd4b5015
Antoine Albertelli <antoine@antoinealb.net>
2016-12-04 15:17:15 +0100

    Enable menu in js

--- download_docset.sh
+++ download_docset.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-wget --mirror http://www.regles-donjons-dragons.com \
+URL=http://www.regles-donjons-dragons.com
+
+wget --mirror $URL \
     --no-host-directories \
     --directory-prefix=dnd.docset/Contents/Resources/Documents
+
+function download_tree_gif() {
+    echo $1
+    wget "$URL/Res/tree/$1" -O dnd.docset/Contents/Resources/Documents/Res/tree/$1
+}
+
+download_tree_gif base.gif
+download_tree_gif folder.gif
+download_tree_gif folderopen.gif
+download_tree_gif page.gif
+download_tree_gif pagesel.gif
+download_tree_gif empty.gif
+download_tree_gif plus.gif
+download_tree_gif minus.gif
+download_tree_gif empty.gif
 
 ./create_index.py
